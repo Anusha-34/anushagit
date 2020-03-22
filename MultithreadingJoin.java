5f0103450b7a617d60cde37b2eab2a00b958d71e
Anusha-34 <62071468+Anusha-34@users.noreply.github.com>
2020-03-23 00:38:32 +0530

    Add files via upload

---
+++ MultithreadingJoin.java
@@ -0,0 +1,30 @@
+
+class MultithreadingJoin
+{
+    public static void main(String args[]) throws Exception
+      { 
+        
+         Thread t1=new Thread(() ->
+              {
+                   for(int i=1;i<=5;i++)
+                     {
+                          System.out.println("Mastek company");
+                          try{ Thread.sleep(1000);}catch(Exception e){}
+                      }
+              });
+         Thread t2=new Thread(() ->
+               {
+                  for(int i=1;i<=5;i++)
+                   {
+                      System.out.println("self learning");
+                      try{ Thread.sleep(1000);}catch(Exception e){}
+                   }
+               });
+         t1.start();
+         try{ Thread.sleep(10);}catch(Exception e){}
+         t2.start();
+         t1.join();
+         t2.join();
+         System.out.println("session modules");
+       }
+}
---
+++ MultithreadingNameM2.java
@@ -0,0 +1,35 @@
+
+class MultithreadingNameM2
+{
+    public static void main(String args[]) throws Exception
+      { 
+        
+         Thread t1=new Thread(() ->
+              {
+                   for(int i=1;i<=5;i++)
+                     {
+                          System.out.println("Mastek company");
+                          try{ Thread.sleep(1000);}catch(Exception e){}
+                      }
+              },"MASTEK THREAD");
+         Thread t2=new Thread(() ->
+               {
+                  for(int i=1;i<=5;i++)
+                   {
+                      System.out.println("self learning");
+                      try{ Thread.sleep(1000);}catch(Exception e){}
+                   }
+               },"LEARNING THREAD");
+         System.out.println(t1.getName());
+         System.out.println(t2.getName());
+         t1.start();
+         try{ Thread.sleep(10);}catch(Exception e){}
+         t2.start();
+         System.out.println(t2.isAlive());
+         t1.join();
+         t2.join();
+         System.out.println(t1.isAlive());
+        
+         System.out.println("session modules");
+       }
+}
